0e1f1b92d9bb0f2549e8f4c54f23efbdd751bc48
Korbinian Maier <korbinian.maier@tum.de>
2022-01-05 22:36:51 +0100

    Fix git git_current_branch

--- zsh/plugins/git/git.plugin.zsh
+++ zsh/plugins/git/git.plugin.zsh
@@ -8,11 +8,11 @@
 # it's not a symbolic ref, but in a Git repo.
 function git_current_branch() {
   local ref
-  ref=$(__git_prompt_git symbolic-ref --quiet HEAD 2> /dev/null)
+  ref=$(git symbolic-ref --quiet HEAD 2> /dev/null)
   local ret=$?
   if [[ $ret != 0 ]]; then
     [[ $ret == 128 ]] && return  # no git repo.
-    ref=$(__git_prompt_git rev-parse --short HEAD 2> /dev/null) || return
+    ref=$(git rev-parse --short HEAD 2> /dev/null) || return
   fi
   echo ${ref#refs/heads/}
 }
