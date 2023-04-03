5eac1a77b4bf3fc16aac3dfcc28b6e468ca88b49
Korbinian Maier <korbinian.maier@posteo.de>
2023-04-03 20:52:13 +0200

    add git plugin

--- install.sh
+++ install.sh
@@ -7,3 +7,6 @@ ln -nfs $cfgdir/zshenv $HOME/.zshenv
 
 # Install zsh cfg folder
 ln -nfs $cfgdir/zsh ${XDG_CONFIG_HOME:=~/.config}/zsh
+
+# Install local git-alias plugin
+ln -nfs $cfgdir/git-alias ~/.zsh-git/git
